cb6a654a69d5dfc2565dccd6a3fa5b0d92aefdce
Thomas Hehn <t.hehn@stud.uni-heidelberg.de>
2017-05-08 13:38:18 +0200

    Initial version.

---
+++ SVGPath2Numeric.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import ipdb
+import argparse
+import json
+from matplotlib import pyplot as plt
+
+def load_json(jsonfile):
+  json_dir = json.load(open(jsonfile, 'r'))
+
+  names = json_dir['data'].keys()
+  data_strs = json_dir['data'].values()
+
+  X_nums = map(lambda x: convert_units(x, json_dir['X1'], json_dir['X2'], 
+                                          axis=0), data_strs)
+  Y_nums = map(lambda x: convert_units(x, json_dir['Y1'], json_dir['Y2'], 
+                                          axis=1), data_strs)
+  X_nums = np.array(list(X_nums))
+  Y_nums = np.array(list(Y_nums))
+
+  return names, X_nums, Y_nums
+
+def parse_path(path):
+  if path[0:2] == 'm ' or path[0:2] == 'M ':
+    path = path[2:]
+  pairs = path.split(' ')
+
+  pairs = [np.array(list(map(float, pair.split(',')))) for pair in pairs]
+  for i in range(1, len(pairs)):
+    pairs[i] += pairs[i-1]
+
+  return np.array(pairs)
+
+def get_Y_refs(Y1, Y2):
+  Y1 = np.array(list(map(float, Y1.split(','))))
+  Y2 = np.array(list(map(float, Y2.split(','))))
+
+  diff = Y2 - Y1
+
+  return Y1, diff
+
+def convert_units(path, ref1, ref2, axis=0):
+  # We actually do not care about X-values pairs[:,0],
+  # because they're the same in all plots.
+  pairs = parse_path(path)
+  ref, diff = get_Y_refs(ref1, ref2)
+
+  s = (pairs[:,axis] - ref[1]) / diff[1]
+  return ref[0] + s * diff[0]
+
+parser = argparse.ArgumentParser(description="Plot score files") 
+parser.add_argument("--json", type=str,
+                    help="Json file.")
+#parser.add_argument("--path", type=str,
+#                    help="Path string.")
+#parser.add_argument("--Y1", type=str,
+#                    help="Y1 ref pair. E.g. 0.6,81.79125")
+#parser.add_argument("--Y2", type=str,
+#                    help="Y2 ref pair. E.g. 0.7,116.35125")
+
+args=parser.parse_args()  
+
+names, Xs, Ys = load_json(args.json)
+
+for name, x, y in zip(names, Xs, Ys):
+  print("---------- {}\nX = {}\nY = {}".format(name, x, y))
+  plt.plot(x, y, label=name)
+plt.legend()
+    
+#Y = convert_Y_units(args.path, args.Y1, args.Y2)
+plt.show()
+#ipdb.set_trace()
